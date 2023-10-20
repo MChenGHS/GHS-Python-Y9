b0503cf13295e1f1515ff5e8c0ced80533dd4479
Mr M Chen <145968094+MChenGHS@users.noreply.github.com>
2023-10-20 10:22:35 +0000

    Bug fixes

--- Oct Assessment/T1-3.py
+++ Oct Assessment/T1-3.py
@@ -1,9 +1,9 @@
 num1 = int(input("Enter a number: "))
-num1 = int(input("Enter a number: "))
-num1 = int(input("Enter a number: "))
-sum = num1 + num2 + num3
+num2 = int(input("Enter a number: "))
+num3 = int(input("Enter a number: "))
+userSum = num1 + num2 + num3
 checkSum = 1230
-if sum != checkSum:
+if userSum != checkSum:
     print("Invalid credentials.")
 else:
     print("Logged in.")
